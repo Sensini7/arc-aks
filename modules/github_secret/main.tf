6d07f82ad810b337eb829fec12593129712b07d7
Sensini7 <pelekengaih@gmail.com>
2025-07-04 06:15:58 +0100

    first commit

--- modules/github_secret/main.tf
+++ modules/github_secret/main.tf
@@ -5,8 +5,8 @@ resource "kubernetes_secret" "github_runner_config" {
   }
 
   data = {
-    github_app_id              = base64encode(var.github_app_id)
-    github_app_installation_id = base64encode(var.github_app_installation_id)
+    github_app_id              = var.github_app_id
+    github_app_installation_id = var.github_app_installation_id
     github_app_private_key     = base64encode(var.github_app_private_key)
   }
 }
