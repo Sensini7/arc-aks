96c5742e79c93d2ba41d411a2776ce2b7a972ea8
Sensini7 <pelekengaih@gmail.com>
2025-07-04 04:14:13 +0100

    first commit

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,11 @@ output "arc_namespace" {
   description = "ARC Namespace"
   value       = module.arc_namespace.namespace_name
 }
+
+output "resource_group_name" {
+  value = module.resource_group.name
+}
+
+output "aks_cluster_name" {
+  value = module.aks.name
+}
