dcb229e1a880aaaa8669bdd024a8ed480e4d6a48
Sensini7 <pelekengaih@gmail.com>
2025-07-04 04:11:03 +0100

    first commit

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,3 +1,9 @@
 output "kube_config" {
   value = azurerm_kubernetes_cluster.this.kube_config
 }
+output "resource_group_name" {
+  value = module.resource_group.name
+}
+output "aks_cluster_name" {
+  value = module.aks.name
+}
